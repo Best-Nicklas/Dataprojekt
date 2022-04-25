2ef52fa9d0c92de1d0bce1d53743e11e158f3770
Markus Kjær <markusnielsen4@gmail.com>
2022-04-25 13:28:58 +0200

    More cleaning, fixing and adding R package folder from devtools

--- ryousick/R/Simulate and open FAM and MAP file.R
+++ ryousick/R/Simulate and open FAM and MAP file.R
@@ -2,14 +2,13 @@
 #'
 #' @param dataframe 
 #' @param MAF 
-#' @param causal_SNP
+#' @param causal_SNPs
 #' @param h2
 #' @param seed 
 #' @return 
 #' @examples   
-#' Simulate_FAM_MAP()
 
-Simulate_FAM_MAP <- function(dataframe, MAF, causal_SNP , h2, seed = NULL) {
+Simulate_FAM_MAP <- function(dataframe, MAF, causal_SNPs , h2, seed = NULL) {
   if (!is.null(seed)) {set.seed(seed)}
   
   rows <- dim(dataframe)[1]
@@ -18,17 +17,19 @@ Simulate_FAM_MAP <- function(dataframe, MAF, causal_SNP , h2, seed = NULL) {
   if (cols != length(MAF)) stop("Number of columns in 
                                  dataframe does not match 
                                  length of MAF vector")
+  c <- sum(causal_SNP == 1)
+  beta <- ifelse(causal_SNP == 1, rnorm(cols, 0, sqrt(h2/c)), 0)
   
-  c <- length(causal_SNP)
-  beta <- numeric(cols)
-  beta[causal_SNP] <- rnorm(length(causal_SNP), 0, sqrt(h2/c))
-  
-  mu <- 2*MAF[causal_SNP]
-  sigma <- sqrt((2*MAF[causal_SNP])*(1-MAF[causal_SNP]))
-  
+  mu <- 2*MAF*causal_SNP 
+  sigma <- sqrt((2*MAF*causal_SNP)*(1-MAF*causal_SNP))
+  sigma[sigma == 0] <- 1 
   liab_g <- numeric(rows)
-  liab_g <- sweep(sweep(dataframe[,causal_SNP], MARGIN = 2, STATS = mu, FUN = "-"), MARGIN = 2, STATS = sigma, FUN = "/") %*%    
-    beta[causal_SNP]
+  
+  b_size <- cols/20
+  for (i in 0:((cols/b_size)-1)) {  
+    b_start <- 1 + i*b_size
+    b_end <- (1+i)*b_size
+    liab_g[b_start:b_end] <- sweep(sweep(dataframe[b_start:b_end, ], MARGIN = 2, STATS = mu, FUN = "-"), MARGIN = 2, STATS = sigma, FUN = "/") %*% beta }
   
   liab_e <- rnorm(dim(dataframe)[1], 0, sqrt(1-h2))
   liab_full <- liab_e + liab_g
@@ -41,7 +42,7 @@ Simulate_FAM_MAP <- function(dataframe, MAF, causal_SNP , h2, seed = NULL) {
   MAP <- tibble(SNP_ID = 1:cols, 
                 Minor_Allele_Frequency = MAF, 
                 BETA = beta, 
-                Causal = ifelse(beta != 0, 1, 0))
+                Causal = causal_SNP)
   
   MAP_and_FAM <- list(genotypes = dataframe, fam = FAM, map = MAP)
   snp_save(MAP_and_FAM)
@@ -52,7 +53,7 @@ Simulate_FAM_MAP <- function(dataframe, MAF, causal_SNP , h2, seed = NULL) {
 #' @param path The path where we have saved the FBM 
 #' @return 
 #' @examples   
-#' OpenRds("../Data/Simulate")
+#' OpenRds("../Data/Simulation_1")
 
 OpenRds <- function(path){
   snp_attach(path)  
